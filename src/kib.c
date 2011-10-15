d78bc2453692fc244c343e7e864e635d070a1f98
Marcel <marcelgmr@gmail.com>
2011-10-15 16:40:18 -0300

    Interactive mode.

--- src/kib.c
+++ src/kib.c
@@ -1,27 +1,66 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 #include "ker.h"
 #include "kob0.h"
 #include "kob1.h"
 #include "krt.h"
 
-int
-main(int argc, char *argv[])
+#define MAX_LINE_LEN 256
+#define MAX_TOKEN_LEN 32
+
+Error
+interactive()
 {
-    char *tokens[] = {"$foo", "$bar", "[+--+-]", "=", "3", "+", "<", "=", ""};
+    char line[MAX_LINE_LEN];
+    char token[MAX_TOKEN_LEN];
+    char *c;
     int i;
+    unsigned int token_id;
     KRuntime *kruntime;
     KBuffer *kbuffer;
+    Error error;
 
     kruntime = newruntime();
     kbuffer = newbuffer();
-    for (i = 0; *tokens[i] != '\0'; i++)
-        kprocess(kruntime, tokens[i]);
-    repnable(kruntime->nable, kbuffer);
-    printf("%s\n", kbuffer->buffer);
 
+    while(1) {
+        printf("> ");
+        if (fgets(line, MAX_LINE_LEN, stdin) == NULL)
+            break;
+        if (strcmp(line, "exit\n") == 0)
+            break;
+        else {
+            token_id = 0;
+            c = line;
+            while (1) {
+                while (isspace(*c) && *c != '\n')
+                    c++;
+                if (*c == '\n')
+                    break;
+                token_id++;
+                for (i = 0; !isspace(*c); i++, c++)
+                    token[i] = *c;
+                token[i] = '\0';
+                error = kprocess(kruntime, token);
+                if (error != E_OK) return error;
+            }
+        repnable(kruntime->nable, kbuffer);
+        printf("%s\n", kbuffer->buffer);
+        }
+    }
     delruntime(&kruntime);
+    delbuffer(&kbuffer);
+    return E_OK;
+}
+
+int
+main(int argc, char *argv[])
+{
+    if (argc == 1)
+        interactive();
 
     return 0;
 }
